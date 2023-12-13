b309dd14b09ad90258a47db48a7995266006e3b0
neerajpatidar05 <neerajpatidar04318@gmail.com>
2023-12-13 13:28:49 +0530

    added total randommint

--- contracts/heart_head/HeartHeads.sol
+++ contracts/heart_head/HeartHeads.sol
@@ -6,10 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract HeartHeads is ERC721, ERC721URIStorage, Ownable {
-     uint256 private _nextTokenId;
+    uint256 private _nextTokenId;
     mapping(uint256 => address) private _tokenOwner;
-    mapping(string => uint256) public tokenLimits; 
-    uint256 public nftprice=0.01 ether;
+    mapping(string => uint256) public tokenLimits;
+    uint256 public nftprice = 0.01 ether;
+    uint256 public totalrandommint;
     string[] private tokenURIs;
 
     event TokenURIsAdded(string[] tokenURIs, uint256[] limits);
@@ -20,7 +21,10 @@ contract HeartHeads is ERC721, ERC721URIStorage, Ownable {
 
     function removeTokenURI(string memory uri) internal {
         for (uint256 i = 0; i < tokenURIs.length; i++) {
-            if (keccak256(abi.encodePacked(tokenURIs[i])) == keccak256(abi.encodePacked(uri))) {
+            if (
+                keccak256(abi.encodePacked(tokenURIs[i])) ==
+                keccak256(abi.encodePacked(uri))
+            ) {
                 delete tokenLimits[uri];
                 tokenURIs[i] = tokenURIs[tokenURIs.length - 1];
                 tokenURIs.pop();
@@ -29,8 +33,14 @@ contract HeartHeads is ERC721, ERC721URIStorage, Ownable {
         }
     }
 
-    function addRandomTokenURIs(string[] memory newTokenURIs, uint256[] memory limits) external onlyOwner {
-        require(newTokenURIs.length == limits.length, "Length mismatch between tokenURIs and limits");
+    function addRandomTokenURIs(
+        string[] memory newTokenURIs,
+        uint256[] memory limits
+    ) external onlyOwner {
+        require(
+            newTokenURIs.length == limits.length,
+            "Length mismatch between tokenURIs and limits"
+        );
         emit TokenURIsAdded(newTokenURIs, limits);
         for (uint256 i = 0; i < newTokenURIs.length; i++) {
             tokenURIs.push(newTokenURIs[i]);
@@ -41,66 +51,67 @@ contract HeartHeads is ERC721, ERC721URIStorage, Ownable {
     function getRandomTokenURI() internal view returns (string memory) {
         require(tokenURIs.length > 0, "No token URIs available");
 
-        uint256 randomIndex = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % tokenURIs.length;
+        uint256 randomIndex = uint256(
+            keccak256(abi.encodePacked(block.timestamp, block.difficulty))
+        ) % tokenURIs.length;
         return tokenURIs[randomIndex];
     }
-    function setNftPrice(uint256 newPrice)  onlyOwner public{
-                nftprice = newPrice;
 
+    function setNftPrice(uint256 newPrice) public onlyOwner {
+        nftprice = newPrice;
     }
-    function safeMintWithRandomTokenURI() external  payable returns (uint256) {
-        require(msg.value>=nftprice, "require 0.01 PLS token to Mint NFT");
+
+    function safeMintWithRandomTokenURI() external payable returns (uint256) {
+        require(msg.value >= nftprice, "require 0.01 PLS token to Mint NFT");
         string memory randomTokenURI = getRandomTokenURI();
         uint256 limit = tokenLimits[randomTokenURI];
 
-       require(limit > 0, "Token limit reached");
+        require(limit > 0, "Token limit reached");
 
         uint256 tokenId = safeMint(randomTokenURI);
+        totalrandommint++;
         tokenLimits[randomTokenURI]--;
-        if(tokenLimits[randomTokenURI]==0){
-                removeTokenURI(randomTokenURI);
+        if (tokenLimits[randomTokenURI] == 0) {
+            removeTokenURI(randomTokenURI);
         }
-          emit TokenMinted(tokenId, randomTokenURI);
+        emit TokenMinted(tokenId, randomTokenURI);
         return tokenId;
     }
 
-    function safeMint(string memory uri) public  returns (uint256) {
+    function safeMint(string memory uri) public returns (uint256) {
         uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
-         _setTokenURI(tokenId, uri);
+        _setTokenURI(tokenId, uri);
         _tokenOwner[tokenId] = msg.sender;
 
         return tokenId;
     }
 
-    function updateMetadataUrl(uint256 tokenId, string memory newMetadataUrl) public  {
-     require(ownerOf(tokenId) == msg.sender, "Not the owner of the token");
+    function updateMetadataUrl(
+        uint256 tokenId,
+        string memory newMetadataUrl
+    ) public {
+        require(ownerOf(tokenId) == msg.sender, "Not the owner of the token");
         _setTokenURI(tokenId, newMetadataUrl);
         emit MetadataUrlUpdated(tokenId, newMetadataUrl);
     }
-    
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return  super.tokenURI(tokenId);
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (bool)
-    {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
-    }   
-function withdrawContractBalance() external onlyOwner {
-    uint256 contractBalance = address(this).balance;
-    require(contractBalance > 0, "Contract balance is zero");
+    }
 
-    payable(owner()).transfer(contractBalance);
-}
+    function withdrawContractBalance() external onlyOwner {
+        uint256 contractBalance = address(this).balance;
+        require(contractBalance > 0, "Contract balance is zero");
 
+        payable(owner()).transfer(contractBalance);
+    }
 }
