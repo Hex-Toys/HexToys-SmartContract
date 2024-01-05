2b901055df4a31373dd2d97e1b16a7b31696467d
riddhishdobariya@logisticinfotech.co.in <rdx446688@gmail.com>
2024-01-05 14:51:13 +0530

    addded hearthead airdrop

---
+++ contracts/airdrophearthead/airdrophearthead.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "../heart_head/HeartHeads.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract AirdropHeartHead is Ownable, IERC721Receiver {
+    address public hearhead;
+
+    constructor(address _hearhead) {
+        hearhead = _hearhead;
+    }
+
+    function airdrop(
+        address[] calldata _user,
+        uint256[] calldata _supply,
+        string calldata _uri
+    ) external {
+        require(_user.length == _supply.length, "Invalid input");
+        for (uint i = 0; i < _user.length; i++) {
+            uint256 tokenId = HeartHeads(hearhead).safeMint(_uri);
+            HeartHeads(hearhead).safeTransferFrom(
+                address(this),
+                _user[i],
+                tokenId
+            );
+        }
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        // Implement your custom logic here
+        // For example, you can log the successful transfer
+        // or perform additional actions
+        emit ERC721Received(operator, from, tokenId, data);
+        return this.onERC721Received.selector;
+    }
+
+    event ERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes data
+    );
+}
---
+++ scripts/airdrophearthead.js
@@ -0,0 +1,55 @@
+const hre = require("hardhat");
+const data_heart = require("../heartheadminter.json");
+const data_hex = require("../hextoysholders.json");
+
+let uri_santa = "ipfs://QmRyb8NQazT5cezX1En8SgY4PDi44bogAWwfv4oNk2iWgq";
+let uri_hexforce = "ipfs://QmUMeMmCQKfLnAZs6c5jQpCT2MUsD9XeUUeKvRccuwyVpd";
+
+async function main() {
+    const heartheadaddress = "0x06d184e3715D0025350cFFc9117a5428036353DF";
+    //const heartheadcontract = await ethers.getContractAt("HeartHeads", heartheadaddress);
+
+    console.log("transaction intiated...")
+    const AirdropHeartHead = await ethers.getContractFactory('AirdropHeartHead');
+    const airdrophearthead = await AirdropHeartHead.deploy(heartheadaddress);
+    await airdrophearthead.deployed()
+
+    console.log(
+        `Airdrop contract deployed to ${airdrophearthead.address}`,
+    );
+    console.log("Starting Airdrop.....");
+    let users = [];
+    let supply = [];
+    for (var i in data_heart) {
+        users.push(data_heart[i].minter);
+        supply.push(1);
+    }
+    for (let i = 0; i < users.length; i += 50) {
+        const chunk_user = users.slice(i, i + 50);
+        const chunk_supply = supply.slice(i, i + 50);
+        let tx = await airdrophearthead.airdrop(chunk_user, chunk_supply, uri_santa);
+        tx.wait(2);
+        console.log(`added ${i} santa hat`);
+    }
+
+    users = [];
+    supply = [];
+    for (var i in data_hex) {
+        users.push(data_hex[i].holderAddress);
+        supply.push(1);
+    }
+    for (let i = 0; i < users.length; i += 50) {
+        const chunk_user = users.slice(i, i + 50);
+        const chunk_supply = supply.slice(i, i + 50);
+        let tx = await airdrophearthead.airdrop(chunk_user, chunk_supply, uri_hexforce);
+        tx.wait(2);
+        console.log(`added ${i} hexforce hat`);
+    }
+    console.log("AirDrop completed......");
+
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
